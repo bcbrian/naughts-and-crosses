3e2528aaa0309f1f84fb78bd1dfe97b9ee8081fa
Brian Bartholomew <me@bcbrian.com>
2019-09-13 21:54:05 -0500

    it uses auth now!

--- assets/js/compManager.js
+++ assets/js/compManager.js
@@ -1,32 +1,70 @@
-function getNCID() {
-  return localStorage.getItem("ncid");
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyCBMFsTwKFAu_hS6qPtIn-kspzlegCTK98",
+  authDomain: "cq-noughts-and-crosses.firebaseapp.com",
+  databaseURL: "https://cq-noughts-and-crosses.firebaseio.com",
+  projectId: "cq-noughts-and-crosses",
+  storageBucket: "cq-noughts-and-crosses.appspot.com",
+  messagingSenderId: "894776809536",
+  appId: "1:894776809536:web:97ce08e818f698a9"
+};
 
-function setNCID(ncid) {
-  return localStorage.setItem("ncid", ncid);
-}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-function createId() {
-  return (
-    Math.random()
-      .toString(36)
-      .substring(2, 15) +
-    Math.random()
-      .toString(36)
-      .substring(2, 15)
-  );
-}
+var actionCodeSettings = {
+  // URL you want to redirect back to. The domain (www.example.com) for this
+  // URL must be whitelisted in the Firebase Console.
+  url: "https://qbsuo.csb.app/finishSignUp",
+  // This must be true.
+  handleCodeInApp: true
+};
 
-function initNCID() {
-  // Check to see if the computer has an identifier for this app
-  let ncid = getNCID();
-  if (ncid) {
-    console.log("Found ID: ", ncid);
-    return;
+// Confirm the link is a sign-in with email link.
+if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
+  // Additional state parameters can also be passed via URL.
+  // This can be used to continue the user's intended action before triggering
+  // the sign-in operation.
+  // Get the email if available. This should be available if the user completes
+  // the flow on the same device where they started it.
+  var email = window.localStorage.getItem("emailForSignIn");
+  if (!email) {
+    // User opened the link on a different device. To prevent session fixation
+    // attacks, ask the user to provide the associated email again. For example:
+    email = window.prompt("Please provide your email for confirmation");
   }
-  ncid = createId();
-  console.log("Storing ID", ncid);
-  setNCID(ncid);
+  // The client SDK will parse the code from the link for you.
+  firebase
+    .auth()
+    .signInWithEmailLink(email, window.location.href)
+    .then(function(result) {
+      // Clear email from storage.
+      window.localStorage.removeItem("emailForSignIn");
+      // You can access the new user via result.user
+      // Additional user info profile not available via:
+      // result.additionalUserInfo.profile == null
+      // You can check if the user is new or existing:
+      // result.additionalUserInfo.isNewUser
+      console.log("result", result);
+    })
+    .catch(function(error) {
+      // Some error occurred, you can inspect the code: error.code
+      // Common errors could be invalid email and invalid or expired OTPs.
+      console.log(error);
+    });
+} else {
+  document.getElementById("RyanJEC").addEventListener("click", function() {
+    const email = document.getElementById("mrssnowymn").value;
+    firebase
+      .auth()
+      .sendSignInLinkToEmail(email, actionCodeSettings)
+      .then(function() {
+        // The link was successfully sent. Inform the user.
+        // Save the email locally so you don't need to ask the user for it again
+        // if they open the link on the same device.
+        window.localStorage.setItem("emailForSignIn", email);
+      })
+      .catch(function(error) {
+        // Some error occurred, you can inspect the code: error.code
+      });
+  });
 }
-
-initNCID();
--- game/index.js
+++ game/index.js
@@ -7,128 +7,142 @@
 const N = "n";
 const C = "c";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCBMFsTwKFAu_hS6qPtIn-kspzlegCTK98",
-  authDomain: "cq-noughts-and-crosses.firebaseapp.com",
-  databaseURL: "https://cq-noughts-and-crosses.firebaseio.com",
-  projectId: "cq-noughts-and-crosses",
-  storageBucket: "cq-noughts-and-crosses.appspot.com",
-  messagingSenderId: "894776809536",
-  appId: "1:894776809536:web:97ce08e818f698a9"
-};
+firebase.auth().onAuthStateChanged(user => {
+  console.log("hi, user", user);
+  if (!user) {
+    document.querySelector(".sign-in-container").style.display = "block";
+  } else {
+    document.querySelector(".sign-in-container").style.display = "none";
+    const db = firebase.firestore();
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+    const urlParams = new URLSearchParams(window.location.search);
+    const gameId = urlParams.get("gameId");
 
-const db = firebase.firestore();
+    function getRandomPosition(num) {
+      return Math.floor(Math.random() * num);
+    }
 
-const urlParams = new URLSearchParams(window.location.search);
-const gameId = urlParams.get("gameId");
+    function renderBoard(nacBoard) {
+      nacBoard.forEach(function(value, i) {
+        document.querySelector(".pos-" + i).innerHTML = value ? value : "";
+      });
+    }
 
-function getRandomPosition(num) {
-  return Math.floor(Math.random() * num);
-}
+    function updateGame(updates) {
+      db.collection("games")
+        .doc(gameId)
+        .update(updates)
+        .then(function() {
+          console.log("Document updated!");
+        })
+        .catch(function(error) {
+          console.error("Error adding document: ", error);
+        });
+    }
 
-function renderBoard(nacBoard) {
-  nacBoard.forEach(function(value, i) {
-    document.querySelector(".pos-" + i).innerHTML = value ? value : "";
-  });
-}
+    db.collection("games")
+      .doc(gameId)
+      .onSnapshot(function(doc) {
+        const docData = doc.data();
+        const turn = docData.turn;
+        const nacBoard = docData.nacBoard;
+        const availablePos = docData.availablePos;
+        const hasWinner = !!docData.winner;
+        const turnId = docData[turn + "Id"];
+        // docdata.nId or cId
+        renderBoard(nacBoard);
 
-function updateGame(updates) {
-  db.collection("games")
-    .doc(gameId)
-    .update(updates)
-    .then(function() {
-      console.log("Document updated!");
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-    });
-}
+        playNoughtsAndCrosses(turnId, turn, nacBoard, availablePos, hasWinner);
+      });
 
-db.collection("games")
-  .doc(gameId)
-  .onSnapshot(function(doc) {
-    const docData = doc.data();
-    const turn = docData.turn;
-    const nacBoard = docData.nacBoard;
-    const availablePos = docData.availablePos;
-    const hasWinner = !!docData.winner;
-    const turnId = docData[turn + "Id"];
-    // docdata.nId or cId
-    renderBoard(nacBoard);
-
-    playNoughtsAndCrosses(turnId, turn, nacBoard, availablePos, hasWinner);
-  });
-
-function playNoughtsAndCrosses(
-  turnId,
-  turn,
-  nacBoard,
-  availablePos,
-  hasWinner
-) {
-  const colEls = document.querySelectorAll(".col");
-  /*************************************************/
-  // TODO: use local storage to give computer an id.
-  /*************************************************/
-  if (hasWinner) {
-    return colEls.forEach(function(colEl) {
-      colEl.onclick = () => {};
-    });
-  }
-  function checkForWin(turn) {
-    if (
-      // check rows
-      (nacBoard[0] === turn && nacBoard[1] === turn && nacBoard[2] === turn) ||
-      (nacBoard[3] === turn && nacBoard[4] === turn && nacBoard[5] === turn) ||
-      (nacBoard[6] === turn && nacBoard[7] === turn && nacBoard[8] === turn) ||
-      // check columns
-      (nacBoard[0] === turn && nacBoard[3] === turn && nacBoard[6] === turn) ||
-      (nacBoard[1] === turn && nacBoard[4] === turn && nacBoard[7] === turn) ||
-      (nacBoard[2] === turn && nacBoard[5] === turn && nacBoard[8] === turn) ||
-      // check diagonal
-      (nacBoard[0] === turn && nacBoard[4] === turn && nacBoard[8] === turn) ||
-      (nacBoard[2] === turn && nacBoard[4] === turn && nacBoard[6] === turn)
+    function playNoughtsAndCrosses(
+      turnId,
+      turn,
+      nacBoard,
+      availablePos,
+      hasWinner
     ) {
-      return turn;
-    }
-    return 0;
-  }
-
-  function playTurn(userPos) {
-    if (availablePos.length < 1) {
-      return;
-    }
-    if (turnId !== getNCID()) {
-      return alert("yo not your turn");
-    }
-    if (!availablePos.includes(userPos)) {
-      return alert("space already taken!");
-    }
-    nacBoard[userPos] = turn;
-    availablePos.splice(availablePos.indexOf(userPos), 1);
-    const winner = checkForWin(turn);
-    updateGame({ nacBoard, availablePos, winner, turn: turn === N ? C : N });
-  }
-
-  // TODO: dumb ai for computer
-  // if (turn === C) {
-  //   const newCross = availablePos[getRandomPosition(availablePos.length)];
-  //   playTurn(newCross);
-  //   return colEls.forEach(function(colEl) {
-  //     colEl.onclick = () => {};
-  //   });
-  // }
-
-  colEls.forEach(function(colEl) {
-    colEl.onclick = function(event) {
-      console.log("HI");
+      const colEls = document.querySelectorAll(".col");
+      /*************************************************/
+      // TODO: use local storage to give computer an id.
+      /*************************************************/
       if (hasWinner) {
-        return;
+        return colEls.forEach(function(colEl) {
+          colEl.onclick = () => {};
+        });
       }
-      playTurn(parseInt(event.target.getAttribute("data-pos"), 10));
-    };
-  });
-}
+      function checkForWin(turn) {
+        if (
+          // check rows
+          (nacBoard[0] === turn &&
+            nacBoard[1] === turn &&
+            nacBoard[2] === turn) ||
+          (nacBoard[3] === turn &&
+            nacBoard[4] === turn &&
+            nacBoard[5] === turn) ||
+          (nacBoard[6] === turn &&
+            nacBoard[7] === turn &&
+            nacBoard[8] === turn) ||
+          // check columns
+          (nacBoard[0] === turn &&
+            nacBoard[3] === turn &&
+            nacBoard[6] === turn) ||
+          (nacBoard[1] === turn &&
+            nacBoard[4] === turn &&
+            nacBoard[7] === turn) ||
+          (nacBoard[2] === turn &&
+            nacBoard[5] === turn &&
+            nacBoard[8] === turn) ||
+          // check diagonal
+          (nacBoard[0] === turn &&
+            nacBoard[4] === turn &&
+            nacBoard[8] === turn) ||
+          (nacBoard[2] === turn && nacBoard[4] === turn && nacBoard[6] === turn)
+        ) {
+          return turn;
+        }
+        return 0;
+      }
+
+      function playTurn(userPos) {
+        if (availablePos.length < 1) {
+          return;
+        }
+        if (turnId !== user.uid) {
+          return alert("yo not your turn");
+        }
+        if (!availablePos.includes(userPos)) {
+          return alert("space already taken!");
+        }
+        nacBoard[userPos] = turn;
+        availablePos.splice(availablePos.indexOf(userPos), 1);
+        const winner = checkForWin(turn);
+        updateGame({
+          nacBoard,
+          availablePos,
+          winner,
+          turn: turn === N ? C : N
+        });
+      }
+
+      // TODO: dumb ai for computer
+      // if (turn === C) {
+      //   const newCross = availablePos[getRandomPosition(availablePos.length)];
+      //   playTurn(newCross);
+      //   return colEls.forEach(function(colEl) {
+      //     colEl.onclick = () => {};
+      //   });
+      // }
+
+      colEls.forEach(function(colEl) {
+        colEl.onclick = function(event) {
+          console.log("HI");
+          if (hasWinner) {
+            return;
+          }
+          playTurn(parseInt(event.target.getAttribute("data-pos"), 10));
+        };
+      });
+    }
+  }
+});
